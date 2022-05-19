f440da1cb2570138fd94b428efd8838d5dab24c5
hung.nguyen <linqtojs@gmail.com>
2022-05-19 09:56:37 +0700

    docs

--- packages/recallback/lib/main.ts
+++ packages/recallback/lib/main.ts
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
-const createCacheItem = (cache: Map<any, any>) => {
+const defaultGetKey = (...args: any[]) => args[0];
+
+const createCacheItem = (cache: Map<any, any>, depth: number) => {
   let currentCallback: Function;
-  let currentGetKey: Function | undefined;
+  let currentGetKey: Function;
 
   return {
     /**
-     * update currentCallback and currentGetKey with latest values
+     * invalidate currentCallback and currentGetKey with latest values
      * @param callback
      * @param getKey
      * @returns
      */
-    update(callback: Function, getKey: Function | undefined) {
+    invalidate(callback: Function, getKey: Function) {
       currentCallback = callback;
       currentGetKey = getKey;
     },
@@ -24,14 +26,14 @@ const createCacheItem = (cache: Map<any, any>) => {
       const result = currentCallback(...args);
       // if the result is function, that means the input is callback factory
       // we should create a cache item for each key
-      if (args.length && typeof result === "function") {
+      if (depth < 3 && typeof result === "function") {
         // the key might be first argument or result of getKey()
-        // user can use getKey() to serialize or generate hash code from list of arguments
-        const key = currentGetKey ? currentGetKey(...args) : args[0];
+        // user can use getKey() to serialize or generate hash code from list of arguments or select which value is the key
+        const key = currentGetKey(...args);
         // create new cache item if not any
         let item = cache.get(key);
         if (!item) {
-          item = createCacheItem(cache);
+          item = createCacheItem(cache, depth + 1);
           cache.set(key, item);
         }
         item.update(result, currentGetKey);
@@ -50,11 +52,22 @@ const createCacheItem = (cache: Map<any, any>) => {
  */
 const useCachedCallback = <T extends (...args: any[]) => any>(
   callback: T,
-  getKey?: (...args: Parameters<T>) => unknown
-): T => {
-  const item = useState(() => createCacheItem(new Map()))[0];
-  item.update(callback, getKey);
-  return item.wrapper as T;
+  getKey: (...args: Parameters<T>) => unknown = defaultGetKey
+) => {
+  const item = useState(() => {
+    const cache = new Map();
+    const { invalidate, wrapper } = createCacheItem(cache, 1);
+    return {
+      invalidate,
+      wrapper: Object.assign(wrapper as T, {
+        cleanup() {
+          cache.clear();
+        },
+      }),
+    };
+  })[0];
+  item.invalidate(callback, getKey);
+  return item.wrapper;
 };
 
 export default useCachedCallback;
